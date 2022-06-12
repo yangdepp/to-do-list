3ce96bf67c4105822de431f26f81959cd974059e
benben-student <a441832620@126.com>
2022-06-12 19:59:22 +0800

    chore:html结构和css布局

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <div className="App"></div>
+      <div className="App">
+        <div className='headerContent'>
+          <button className='bto1'>all</button>
+          <button className='bto1'>actived</button>
+          <button className='bto1'>completed</button>
+        </div>
+        <div className='bodyContent'></div>
+
+          <span className='left'>items left</span>
+          <span className='right'>Clear completed</span>
+
+      </div>
     </div>
   );
 }
