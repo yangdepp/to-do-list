0ee876894a15ee40881abd55fa36a36530c4cab9
benben-student <a441832620@126.com>
2022-06-13 22:05:38 +0800

    feat:html结构和js功能

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import './App.css';
 
+
 function App() {
   return (
     <div>
       <div className="App">
         <div className='headerContent'>
-          <button className='bto1'>all</button>
-          <button className='bto1'>actived</button>
-          <button className='bto1'>completed</button>
+          <span className='bto1'>all</span>
+          <span className='bto1'>actived</span>
+          <span className='bto1'>completed</span>
+        </div>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
         </div>
         <div className='bodyContent'></div>
-
-          <span className='left'>items left</span>
-          <span className='right'>Clear completed</span>
-
+        <span className='left'>items left</span>
+        <span className='right'>Clear completed</span>
       </div>
     </div>
   );
 }
+function Lable(){
 
+   const box=document.getElementsByClassName('.App')
+   console.log(box)
+   return 
+}
+Lable()
 export default App;
