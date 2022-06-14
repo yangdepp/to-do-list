aa1301a517dcbd1a6b5bfe220d39bf20af488a68
benben-student <a441832620@126.com>
2022-06-14 16:52:15 +0800

    feat:Todolist增加功能

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
+import React from 'react';
 import './App.css';
 
 
-function App() {
-  return (
-    <div>
-      <div className="App">
-        <div className='headerContent'>
-          <span className='bto1'>all</span>
-          <span className='bto1'>actived</span>
-          <span className='bto1'>completed</span>
-        </div>
-        <div>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <div className='bodyContent'></div>
-        <span className='left'>items left</span>
-        <span className='right'>Clear completed</span>
-      </div>
-    </div>
-  );
-}
-function Lable(){
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: [],
+            inputValue: ''
+        }
+    }
+    spanChangeOne() {
+        this.setState({
+            list: [...this.state.list, '123']
+        })
+    }
+    spanChangeTwo() {
+        this.setState({
+            list: [...this.state.list, '456']
+        })
+    }
+    spanChangeThree() {
+        this.setState({
+            list: [...this.state.list, '789']
+        })
+    }
+    handleBtnChange() {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
+    }
+    inputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div className="App">
+                    <div className='headerContent'>
+                        <span value={this.state.spanValue} className='bto1' onClick={this.spanChangeOne.bind(this)}>all</span>
+                        <span value={this.state.spanValue} className='bto1' onClick={this.spanChangeTwo.bind(this)}>actived</span>
+                        <span value={this.state.spanValue} className='bto1' onClick={this.spanChangeThree.bind(this)}>completed</span>
 
-   const box=document.getElementsByClassName('.App')
-   console.log(box)
-   return 
+                        <input value={this.state.inputValue} onChange={this.inputChange.bind(this)} /><button onClick={this.handleBtnChange.bind(this)}>add</button>
+                    </div>
+                    <div className='header-foot'>
+                        <ul>
+                            {this.state.list.map((item, index) => {
+                                return <li key={index}>{item}</li>
+                            })}
+                        </ul>
+                    </div>
+                    <div className='bodyContent'></div>
+                    <span className='left'>items left</span>
+                    <span className='right'>Clear completed</span>
+                </div>
+            </div>
+        );
+    }
 }
-Lable()
+
 export default App;
---
+++ src/Todolist.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+
+class Todolist extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: [],
+             inputValue: ''
+        }
+    }
+    handleBtnClick() {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
+    }
+    handleInputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} />
+                    <button onClick={this.handleBtnClick.bind(this)}>add</button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return <li key={index}>{item}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
+export default Todolist;
